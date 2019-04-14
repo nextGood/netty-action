276cfd41920eecf361af34bf5e0b784312ace45e
nextGood <783774089@qq.com>
2019-04-15 07:44:12 +0800

    0415

--- src/main/java/nio/TimeServerAio.java
+++ src/main/java/nio/TimeServerAio.java
@@ -1,5 +1,10 @@
 package nio;
 
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * AIO Server
  *
@@ -8,6 +13,47 @@ package nio;
  */
 public class TimeServerAio {
     public static void main(String[] args) {
+        int port = 8080;
+        try {
+            if (null != args && args.length > 0) {
+                port = Integer.valueOf(args[0]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        AsyncTimeServerHandler timeServerHandler = new AsyncTimeServerHandler(port);
+        new Thread(timeServerHandler, "AsyncTimeServer").start();
+    }
 
+    private static class AsyncTimeServerHandler implements Runnable {
+        private int port;
+        private CountDownLatch countDownLatch;
+        private AsynchronousServerSocketChannel serverSocketChannel;
+
+        public AsyncTimeServerHandler(int port) {
+            this.port = port;
+            try {
+                serverSocketChannel = AsynchronousServerSocketChannel.open();
+                serverSocketChannel.bind(new InetSocketAddress(port));
+                System.out.println("The time server is start in port : " + port);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            countDownLatch = new CountDownLatch(1);
+            doAccept();
+            try {
+                countDownLatch.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void doAccept() {
+            //serverSocketChannel.accept(this, new AcceptCompletionHandler());
+        }
     }
 }
